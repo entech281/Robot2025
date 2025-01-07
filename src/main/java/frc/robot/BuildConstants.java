05caa84c27a219785460837c3e0300a7d0befb15
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2025-01-06 19:07:27 -0500

    stuff ready maybe idk

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Robot2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "76e31d6f9d2919cc52de5fad02cc48b47533b1c5";
+  public static final String GIT_DATE = "2025-01-06 16:50:38 EST";
+  public static final String GIT_BRANCH = "AH_pull_in_old_code";
+  public static final String BUILD_DATE = "2025-01-06 19:07:13 EST";
+  public static final long BUILD_UNIX_TIME = 1736208433873L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
