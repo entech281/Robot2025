15a06d8c33645c83fc3837b5484535efd2b739bf
Paul <paulmanavalan1@gmail.com>
2025-01-07 19:28:28 -0500

    Added stuff to main.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "05caa84c27a219785460837c3e0300a7d0befb15";
-  public static final String GIT_DATE = "2025-01-06 19:07:27 EST";
-  public static final String GIT_BRANCH = "AH_pull_in_old_code";
-  public static final String BUILD_DATE = "2025-01-07 01:17:12 EST";
-  public static final long BUILD_UNIX_TIME = 1736230632497L;
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "f346f2edd5b3c03a50ed4688c035ffd2f26994c5";
+  public static final String GIT_DATE = "2025-01-07 19:02:21 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-07 19:18:19 EST";
+  public static final long BUILD_UNIX_TIME = 1736295499923L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
