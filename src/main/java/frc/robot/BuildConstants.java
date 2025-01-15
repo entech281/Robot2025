43db29dfab0edd27a00fe04136a5b18ce64b0f4c
Aarush Bhatnagar <ninjazard29@gmail.com>
2025-01-14 20:00:20 -0500

    basic auto

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 51;
-  public static final String GIT_SHA = "1cf5a7e2db44f8e7b35c344d103ce64e647a829d";
-  public static final String GIT_DATE = "2025-01-12 19:59:16 EST";
-  public static final String GIT_BRANCH = "aprilTagDistance";
-  public static final String BUILD_DATE = "2025-01-12 20:01:19 EST";
-  public static final long BUILD_UNIX_TIME = 1736730079164L;
+  public static final int GIT_REVISION = 53;
+  public static final String GIT_SHA = "4902a572e71ca6b5ae1643f0529a66e7268a6dab";
+  public static final String GIT_DATE = "2025-01-12 20:08:05 EST";
+  public static final String GIT_BRANCH = "ab_very_basic_auto";
+  public static final String BUILD_DATE = "2025-01-14 19:58:58 EST";
+  public static final long BUILD_UNIX_TIME = 1736902738118L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
