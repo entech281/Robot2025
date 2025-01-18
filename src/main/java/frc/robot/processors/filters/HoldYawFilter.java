7dbf0648e6d008c0e3e828c2d666cae7ba09d82a
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2025-01-18 14:36:17 -0500

    fixes

--- src/main/java/frc/robot/processors/filters/HoldYawFilter.java
+++ src/main/java/frc/robot/processors/filters/HoldYawFilter.java
@@ -1,7 +1,5 @@
 package frc.robot.processors.filters;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.entech.util.StoppingCounter;
@@ -9,7 +7,7 @@ import frc.robot.operation.UserPolicy;
 import frc.robot.subsystems.drive.DriveInput;
 
 public class HoldYawFilter implements DriveFilterI {
-  private final static double RESET_MARGIN = 3.0;
+  private static final double RESET_MARGIN = 3.0;
   private final PIDController controller = new PIDController(0.0035, 0, 0.0);
   private final StoppingCounter stopCounter = new StoppingCounter(0.25);
   private Rotation2d holdAngle = new Rotation2d(0);
@@ -22,12 +20,9 @@ public class HoldYawFilter implements DriveFilterI {
   public DriveInput process(DriveInput input) {
     DriveInput filteredInput = new DriveInput(input);
 
-    if (holdAngle != null)
-      Logger.recordOutput("Hold Angle", holdAngle.getDegrees());
-
     if (!stopCounter.isFinished(input.getRotation() == 0.0) || (Math.abs(input.getLatestOdometryPose().getRotation().getDegrees() - holdAngle.getDegrees()) > RESET_MARGIN)) {
       holdAngle = input.getLatestOdometryPose().getRotation();
-    } else if (holdAngle != null && !UserPolicy.getInstance().isTwistable()) {
+    } else if (!UserPolicy.getInstance().isTwistable()) {
       filteredInput.setRotation(controller.calculate(
           input.getLatestOdometryPose().getRotation().getDegrees(), holdAngle.getDegrees()));
     }
