a7fcee6c74a8adcf32d6f69d5bee48c520fa96d6
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2025-01-18 17:14:31 -0500

    fixes

--- src/main/java/frc/robot/processors/OdometryProcessor.java
+++ src/main/java/frc/robot/processors/OdometryProcessor.java
@@ -1,11 +1,8 @@
 package frc.robot.processors;
 
-import java.util.Optional;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotConstants;
