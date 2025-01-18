82074b1faac54f7854ef18af0d6b6793b6301573
tadpoleAngel <opc4775@gmail.com>
2025-01-17 22:16:47 -0500

    making new branch

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 14;
-  public static final String GIT_SHA = "8ec723019219b4c5d36ebba5a596478656ff6b10";
-  public static final String GIT_DATE = "2025-01-07 19:35:04 EST";
+  public static final int GIT_REVISION = 15;
+  public static final String GIT_SHA = "87103f07d33a33faa7af22d2c037d582e5759302";
+  public static final String GIT_DATE = "2025-01-07 20:06:59 EST";
   public static final String GIT_BRANCH = "oc_copy_old_tests";
-  public static final String BUILD_DATE = "2025-01-07 20:06:43 EST";
-  public static final long BUILD_UNIX_TIME = 1736298403772L;
+  public static final String BUILD_DATE = "2025-01-13 19:35:00 EST";
+  public static final long BUILD_UNIX_TIME = 1736814900012L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
