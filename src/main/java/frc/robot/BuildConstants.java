64856866b33131381c660523b8f3028a4ea96275
tadpoleAngel <opc4775@gmail.com>
2025-01-23 18:51:44 -0500

    changed vision parameters

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 55;
-  public static final String GIT_SHA = "bae3893e40511eb9f524cfefe7ca39610c95feac";
-  public static final String GIT_DATE = "2025-01-14 19:20:21 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-17 17:12:57 EST";
-  public static final long BUILD_UNIX_TIME = 1737151977175L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 63;
+  public static final String GIT_SHA = "048a0e7308de156946fe8ef39b0c88ab54f9aaa0";
+  public static final String GIT_DATE = "2025-01-20 16:20:46 EST";
+  public static final String GIT_BRANCH = "oc_vision";
+  public static final String BUILD_DATE = "2025-01-22 19:12:37 EST";
+  public static final long BUILD_UNIX_TIME = 1737591157025L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
