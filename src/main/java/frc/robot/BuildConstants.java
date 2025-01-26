54653e8bf5211e0ee0f03da5d394ca1b3a123e6b
tadpoleAngel <opc4775@gmail.com>
2025-01-26 16:39:15 -0500

    WPILib update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 63;
-  public static final String GIT_SHA = "048a0e7308de156946fe8ef39b0c88ab54f9aaa0";
-  public static final String GIT_DATE = "2025-01-20 16:20:46 EST";
+  public static final int GIT_REVISION = 100;
+  public static final String GIT_SHA = "bb1f5feba5b684065f27404084f92515aed80186";
+  public static final String GIT_DATE = "2025-01-24 16:47:31 EST";
   public static final String GIT_BRANCH = "oc_vision";
-  public static final String BUILD_DATE = "2025-01-22 19:12:37 EST";
-  public static final long BUILD_UNIX_TIME = 1737591157025L;
+  public static final String BUILD_DATE = "2025-01-26 16:35:58 EST";
+  public static final long BUILD_UNIX_TIME = 1737927358006L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
