c44a78cc1d986f3899bbaa0c37867b304840cf75
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2025-01-30 19:32:36 -0500

    save tuning

--- src/main/java/frc/robot/commands/RotateToFaceTargetCommand.java
+++ src/main/java/frc/robot/commands/RotateToFaceTargetCommand.java
@@ -5,8 +5,8 @@ import frc.robot.io.RobotIO;
 import frc.robot.operation.UserPolicy;
 
 public class RotateToFaceTargetCommand extends EntechCommand {
-    private static final double KP = 5.0;
-    public static final double TOLERANCE = 0.1;
+    private static final double KP = 7.0;
+    public static final double TOLERANCE = 2.5;
 
     @Override
     public void end(boolean interrupted) {
@@ -15,8 +15,10 @@ public class RotateToFaceTargetCommand extends EntechCommand {
 
     @Override
     public void execute() {
-        double change = KP * RobotIO.getInstance().getVisionOutput().getTagXP();
-        UserPolicy.getInstance().setTargetAngle(RobotIO.getInstance().getOdometryPose().getRotation().getDegrees() - change);
+        if (RobotIO.getInstance().getVisionOutput().getTagXP() >= -99) {
+            double change = KP * RobotIO.getInstance().getVisionOutput().getTagXP();
+            UserPolicy.getInstance().setTargetAngle(RobotIO.getInstance().getOdometryPose().getRotation().getDegrees() - change);
+        }
     }
 
     @Override
