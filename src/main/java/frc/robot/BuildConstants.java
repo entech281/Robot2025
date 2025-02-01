db0acb0e010bcd0aad2247059d60c0241caf7ff1
Aarush Bhatnagar <ninjazard29@gmail.com>
2025-02-01 13:32:29 -0500

    optimal camera settings

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Robot2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 51;
-  public static final String GIT_SHA = "1cf5a7e2db44f8e7b35c344d103ce64e647a829d";
-  public static final String GIT_DATE = "2025-01-12 19:59:16 EST";
-  public static final String GIT_BRANCH = "aprilTagDistance";
-  public static final String BUILD_DATE = "2025-01-12 20:01:19 EST";
-  public static final long BUILD_UNIX_TIME = 1736730079164L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 55;
+  public static final String GIT_SHA = "bae3893e40511eb9f524cfefe7ca39610c95feac";
+  public static final String GIT_DATE = "2025-01-14 19:20:21 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2025-01-21 18:29:40 EST";
+  public static final long BUILD_UNIX_TIME = 1737502180737L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
