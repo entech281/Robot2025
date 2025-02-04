73ae6d1e2db6ed5c5951243cb237560eb784c870
Paul <paulmanavalan1@gmail.com>
2025-02-03 19:23:53 -0500

    testing to fix bug

---
+++ src/main/java/frc/robot/subsystems/coralMechanism/CoralMechanismInput.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems.coralMechanism;
+
+import org.littletonrobotics.junction.LogTable;
+
+import frc.entech.subsystems.SubsystemInput;
+import frc.robot.RobotConstants;
+
+public class CoralMechanismInput implements SubsystemInput {
+
+private boolean activate = true;
+  private double requestedPosition = RobotConstants.ELEVATOR.INITIAL_POSITION;
+
+  @Override
+  public void toLog(LogTable table) {
+    table.put("Activate", activate);
+    table.put("Requested position", requestedPosition);
+  }
+
+  @Override
+  public void fromLog(LogTable table) {
+    activate = table.get("Activate", activate);
+    requestedPosition = table.get("Requested position", 0.0);
+  }
+
+  public boolean getActivate() {
+    return this.activate;
+  }
+
+  public void setActivate(boolean activate) {
+    this.activate = activate;
+  }
+
+  public double getRequestedPosition() {
+    return this.requestedPosition;
+  }
+
+  public void setRequestedPosition(double requestedPosition) {
+    this.requestedPosition = requestedPosition;
+  }
+}   
---
+++ src/main/java/frc/robot/subsystems/coralMechanism/CoralMechanismSubsystem.java
@@ -0,0 +1,49 @@
+package frc.robot.subsystems.coralMechanism;
+
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+import frc.entech.subsystems.EntechSubsystem;
+import frc.robot.io.RobotIO;
+
+public class CoralMechanismSubsystem extends EntechSubsystem<CoralMechanismInput, CoralMechanismOutput> {
+    private static final boolean ENABLED = true;
+    private static final boolean IS_INVERTED = false;
+    
+    private CoralMechanismInput currentInput = new CoralMechanismInput();
+
+    private SparkMax CoralMotor;
+
+    public void initialize() {
+        if (ENABLED) {
+            CoralMotor = new SparkMax(0, SparkMax.MotorType.kBrushless);
+            CoralMotor.getEncoder().setPosition(0.0);
+            CoralMotor.setInverted(IS_INVERTED);
+        }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return ENABLED;
+    }
+    
+
+    @Override
+    public void updateInputs(CoralMechanismInput input) {
+        RobotIO.processInput(input);
+        this.currentInput = input;
+    }
+
+    @Override
+    public Command getTestCommand() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTestCommand'");
+    }
+
+    @Override
+    public CoralMechanismOutput toOutputs() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toOutputs'");
+    }
+}
