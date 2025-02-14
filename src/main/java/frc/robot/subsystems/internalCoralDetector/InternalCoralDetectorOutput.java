72fe11bbb6bcc98a0bc14af9cfa03e4505fa070d
Aarush Bhatnagar <ninjazard29@gmail.com>
2025-02-13 19:06:18 -0500

    class to detect note inside the funnel

---
+++ src/main/java/frc/robot/subsystems/internalCoralDetector/InternalCoralDetectorOutput.java
@@ -0,0 +1,38 @@
+package frc.robot.subsystems.internalCoralDetector;
+
+import org.littletonrobotics.junction.Logger;
+import frc.entech.subsystems.SubsystemOutput;
+
+public class InternalCoralDetectorOutput extends SubsystemOutput {
+
+  private boolean forwardCoralSensor = false;
+  private boolean rearCoralSensor = false;
+
+  public boolean forwardSensorHasCoral() {
+    return forwardCoralSensor;
+  }
+
+  public void setForwardCoralSensor(boolean forwardCoralSensor) {
+    this.forwardCoralSensor = forwardCoralSensor;
+  }
+
+  public boolean rearSensorHasCoral() {
+    return rearCoralSensor;
+  }
+
+  public void setRearCorealSensor(boolean rearCoralSensor) {
+    this.rearCoralSensor = rearCoralSensor;
+  }
+
+  public boolean hasCoral() {
+    return forwardCoralSensor || rearCoralSensor;
+  }
+
+  @Override
+  public void toLog() {
+    Logger.recordOutput("InternalCoralDetectorOutput/ForwardInternalSensor", forwardCoralSensor);
+    Logger.recordOutput("InternalCoralDetectorOutput/RearInternalSensor", rearCoralSensor);
+    Logger.recordOutput("InternalCoralDetectorOutput/HasCoral", hasCoral());
+  }
+
+}
