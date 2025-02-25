8f63450cbaab857115b80c2e9e328c8883130590
Aarush Bhatnagar <ninjazard29@gmail.com>
2025-02-24 19:02:08 -0500

    fixed some numbers

--- src/main/java/frc/robot/commands/ResetOdometryCommand.java
+++ src/main/java/frc/robot/commands/ResetOdometryCommand.java
@@ -1,9 +1,6 @@
 package frc.robot.commands;
 
-import java.util.Optional;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import frc.entech.commands.EntechCommand;
 import frc.robot.processors.OdometryProcessor;
 
@@ -16,11 +13,6 @@ public class ResetOdometryCommand extends EntechCommand {
 
   @Override
   public void initialize() {
-    Optional<Alliance> team = DriverStation.getAlliance();
-    if (team.isPresent() && team.get() == Alliance.Blue) {
-      odometry.resetOdometry(new Pose2d(1.38, 5.53, odometry.getEstimatedPose().getRotation()));
-      return;
-    }
     odometry.resetOdometry(new Pose2d(15.17, 5.53, odometry.getEstimatedPose().getRotation()));
   }
 
