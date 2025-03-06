45e1ed91edcced8963d1ccca66a30fcddc45d894
Aarush Bhatnagar <ninjazard29@gmail.com>
2025-03-05 19:08:01 -0500

    intake and fire for algae

---
+++ src/main/java/frc/robot/commands/IntakeAlgaeCommand.java
@@ -0,0 +1,40 @@
+package frc.robot.commands;
+
+import frc.entech.commands.EntechCommand;
+import frc.entech.util.StoppingCounter;
+import frc.robot.io.RobotIO;
+import frc.robot.subsystems.coralmechanism.CoralMechanismInput;
+import frc.robot.subsystems.coralmechanism.CoralMechanismOutput;
+import frc.robot.subsystems.coralmechanism.CoralMechanismSubsystem;
+
+public class IntakeAlgaeCommand extends EntechCommand {
+    private final CoralMechanismInput input = new CoralMechanismInput();
+    private final CoralMechanismSubsystem intake;
+
+    public IntakeAlgaeCommand(CoralMechanismSubsystem algae) {
+        super(algae);
+        this.intake = algae;
+    }
+
+	@Override
+	public void end(boolean interrupted) {
+		input.setRequestedSpeed(-0.05);
+		intake.updateInputs(input);
+	}
+
+	@Override
+	public void execute() {
+    //Nothing to execute
+	}
+
+	@Override
+	public void initialize() {
+		input.setRequestedSpeed(-0.2);
+		intake.updateInputs(input);
+	}
+
+	@Override
+	public boolean isFinished() {
+    return intake.toOutputs().getHasAlgae();
+	}
+}
