6ba77e2ce697ca9b1b8920e3aac39abb6e321a3e
WhyDoesGodDoThis <101780103+WhyDoesGodDoThis@users.noreply.github.com>
2025-03-07 15:56:12 -0500

    testing

--- src/main/java/frc/robot/CommandFactory.java
+++ src/main/java/frc/robot/CommandFactory.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ProxyCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -124,6 +125,8 @@ public class CommandFactory {
 
     autoChooser = AutoBuilder.buildAutoChooser();
     SmartDashboard.putData("Auto Chooser", autoChooser);
+
+    subsystemManager.getElevatorSubsystem().setTestCommand(Commands.deferredProxy(() -> { return formSafeMovementCommand(Position.L2); }));
   }
 
   public Command getAutoCommand() {
